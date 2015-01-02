ad61042b2bae6e6961423783c2bb8221b661a338
mysticPrg <mysticPrg@gmail.com>
2015-01-03 04:00:58 +0900

    modify Dockerfile

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 /**
- * Created by mysticprg on 15. 1. 2.
- */
+* Created by mysticprg on 15. 1. 2.
+*/
 
 var app = require('express')();
 
 app.get('/', function(req, res) {
-    res.send('Hello Docker World!');
+    res.send('Hello Docker World! with Auto Deploy!');
 });
 
 app.listen(8080);
+
+//var exec = require('child_process').execFile;
+//
+//var child = exec('./restart.sh', function (err, stdout, stderr) {
+//    console.log('stdout: ' + stdout);
+//    console.log('stderr: ' + stderr);
+//    if ( err ) {
+//        console.log('exec err: ' + err);
+//    }
+//});
