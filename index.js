05f942087f3c1e8615dcd9aac728d4c1a69a0e77
mysticPrg <mysticPrg@gmail.com>
2015-01-03 05:41:57 +0900

    add send

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
 var app = require('express')();
 
 app.get('/', function(req, res) {
-    res.send('I Missing U jenny...');
-    res.send('Hello Docker World! with Auto Deploy Tool from mysticPrg! :)');
+    res.send('I Missing U jenny... from mysticPrg T-T');
 });
 
 app.listen(8080);
