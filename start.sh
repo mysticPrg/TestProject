153250ffd7989ed7bc0a09ab43c9e6ca3aa62566
mysticPrg <mysticPrg@gmail.com>
2015-01-04 01:17:53 +0900

    Create dynamic development env

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build --tag mysticprg/testproject .
-docker run -d -p 8123:8080 --name test mysticprg/testproject
+docker run -d -p 8123:8080 --name test -v $(pwd):/home/TestProject mysticprg/testproject
