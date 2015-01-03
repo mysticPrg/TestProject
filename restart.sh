aac3b3c2f220a89ac3f11ff77b617a1f1bad2cd8
mysticPrg <mysticPrg@gmail.com>
2015-01-04 07:48:41 +0900

    add remove non tag images comand to sh files

--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@
 docker stop test
 docker build --tag mysticprg/testproject .
 docker rm test
-#docker run -d -p 8123:8080 --name test mysticprg/testproject
 docker run -d -p 8123:8080 --name test -v $(pwd) /home/TestProject mysticprg/testproject
+docker rmi $(docker images -f "dangling=true" -q)
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker build --tag mysticprg/testproject .
 docker run -d -p 8123:8080 --name test -v $(pwd):/home/TestProject mysticprg/testproject
+docker rmi $(docker images -f "dangling=true" -q)
