ebc9fa0e27b889fff178729d707c8a11f9dec162
mysticPrg <mysticPrg@gmail.com>
2015-01-03 15:07:00 +0900

    create static server

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 /**
-* Created by mysticprg on 15. 1. 2.
-*/
+ * Created by mysticprg on 15. 1. 2.
+ */
 
-var app = require('express')();
+var express = require('express');
+var app = express();
 
-app.get('/', function(req, res) {
-    res.send('I Missing U jenny... from mysticPrg T-T...');
+//app.get('/', function(req, res) {
+//    res.send('I Missing U jenny... from mysticPrg T-T...');
+//});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (res, req) {
+   res.redirect('/public/index.html');
 });
 
-app.listen(8080);
+app.listen(8080, function () {
+    console.log('Server is Running...');
+});
